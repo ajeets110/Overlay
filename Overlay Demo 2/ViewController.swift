b3c62edd00c2f0f3c0411c0abce5cf961488e79c
Ajeet Singh <ajeets110@gmail.com>
2020-01-10 16:11:10 -0500

    init

--- Overlay Demo 2/ViewController.swift
+++ Overlay Demo 2/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import MapKit
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
+    let places = Place.getPlaces()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        //locationManager.startUpdatingLocation()
+        
+        addAnnotation()
     }
 
+    func addAnnotation(){
+        mapView.delegate = self
+        mapView.addAnnotations(places)
+        
+        let overlay = places.map{
+            MKCircle(center: $0.coordinate, radius: 200)
+        }
+        mapView.addOverlays(overlay)
+    }
 
 }
 
+extension ViewController: MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation{
+            return nil
+            
+        } else {
+            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
+            annotationView.image = UIImage(named: "ic_place_2x.png")
+            return annotationView
+        }
+    }
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKCircleRenderer(overlay: overlay)
+        renderer.fillColor = UIColor.black.withAlphaComponent(0.5)
+        renderer.strokeColor = UIColor.green
+        return renderer
+    }
+}
