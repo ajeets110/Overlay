ee911c0646878d509e6614692f7266a7a93ea0fc
Ajeet Singh <ajeets110@gmail.com>
2020-01-10 15:19:22 -0500

    Initial Commit

---
+++ Overlay Demo 2Tests/Overlay_Demo_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Overlay_Demo_2Tests.swift
+//  Overlay Demo 2Tests
+//
+//  Created by MacStudent on 2020-01-10.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import XCTest
+@testable import Overlay_Demo_2
+
+class Overlay_Demo_2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
